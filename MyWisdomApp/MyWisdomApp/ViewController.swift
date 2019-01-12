ba6a8e17c24f3bc9f97dfa41957191405a7ca517
Filippo <filippodaminato@gmail.com>
2019-01-12 12:26:37 +0100

    Add xcode project

---
+++ MyWisdomApp/MyWisdomApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWisdomApp
+//
+//  Created by Filippo Daminato on 12/01/2019.
+//  Copyright © 2019 Filippo Daminato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
